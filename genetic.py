0a58253b35fe6cbb3b331ff6b16e19572f128e32
Ben Nageris <bennageris@gmail.com>
2023-05-18 08:05:54 +0300

    use enc.txt

--- genetic.py
+++ genetic.py
@@ -329,11 +329,9 @@ class DarwinSolver(Solver):
 
 if __name__ == "__main__":
     dictionary = EnglishDictionary('dict.txt', 'Letter2_Freq.txt', 'Letter_Freq.txt')
-    txt = """Pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba."""
+    # txt = """Pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba."""
+    with open(r"enc.txt", "r") as f:
+        txt = f.read()
     solver = NormalSolver(population_size=1000, text=txt, english_dictionary=dictionary)
-
-    # for generation_fitness_permutation, generation_fitness_score in generation_fitness_list:
-    #     print(generation_fitness_score)
     solver.solve(400)
-    # print([fit for _, fit in solver._generation])
     print(solver._best_sol.translate(txt))
